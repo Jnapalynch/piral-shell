ee4e5e3beeae84c5ae1396012d020b57f2ad5198
jnapa <jnapalyn@everis.com>
2020-02-12 15:52:30 -0500

    feed url was changed

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { layout } from './custom-layout';
 
 // change to your feed URL here (either using feed.piral.io or your own service)
 // const feedUrl = 'https://feed.piral.io/api/v1/pilet/store-feed';
-const feedUrl = 'http://127.0.0.1:9000/';
+const feedUrl = 'http://34.231.208.232/';
 
 renderInstance({
   layout,
